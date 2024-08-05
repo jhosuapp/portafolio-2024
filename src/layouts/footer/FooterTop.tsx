97aa0e1b0d0f9b7085b8fabcbd9222bccd15a2ed
jhosuapp <jhosuapvll@gmail.com>
2024-08-04 19:58:28 -0500

    feat: footer information

--- src/layouts/footer/FooterTop.tsx
+++ src/layouts/footer/FooterTop.tsx
@@ -2,9 +2,9 @@ const FooterTop = ():JSX.Element => {
     return (
         <section className="footer__information">
             <ul>
-                <li>©2024 JHOSUAPP</li>
+                <li>©2024 <a href="https://github.com/jhosuapp" target="_blank">jhosuapp</a></li>
                 <li>R&D:labs.usion.co</li>
-                <li>Built by <a href="#">jhosuapp</a> with❤️</li>
+                <li>Built by <a href="https://github.com/jhosuapp" target="_blank">jhosuapp</a> with ❤️</li>
             </ul>
         </section>
     )
