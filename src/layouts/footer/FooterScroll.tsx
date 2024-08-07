63ef67c931a5feda1f6d29c841507ab7afcab03a
jhosuapp <jhosuapvll@gmail.com>
2024-08-06 21:33:23 -0500

    feat: scroll footer to next page

--- src/layouts/footer/FooterScroll.tsx
+++ src/layouts/footer/FooterScroll.tsx
@@ -1,31 +1,58 @@
 
 
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useMediaQuery } from "../../hooks/";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const FooterScroll = ():JSX.Element => {
 
     const isDesk = useMediaQuery({breakpoint: 991 });
     const location = useLocation();
+    const [counter, setCounter] = useState<number>(0);
+    const node = useRef<HTMLElement>(null);
+    const navigate = useNavigate();
+    //Wheel
+    const HandleWheel = ()=>{
+        setCounter(counter + 2);
+    }
+    //Observe footer
+    useEffect(()=>{
+        //Obsever
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    counter < 100 && window.addEventListener('wheel', HandleWheel);
+                }else {
+                    setCounter(0);
+                }
+            })
+        });
+        //Validate
+        if(counter === 100){
+            setCounter(0);
+            location.pathname === '/contact' ? navigate('/') : navigate('/contact');
+        }
 
-    // useEffect(()=>{
-    //     window.addEventListener('wheel', (e)=>{
-    //         console.log(e);
-    //     })
-    // },[]);
+        node.current && observer.observe(node.current);
 
+        return ()=>{
+            observer.disconnect();
+            window.removeEventListener('wheel', HandleWheel);
+        }
+    },[counter]);
 
     return (
-        <section className="footer__scroll">
+        <section ref={ node } className="footer__scroll">
             <article>
                 <p className="footer__scroll__text">Keep scrolling to learn more</p>
             </article>
             <article>
-                <p>{location.pathname == '/contact' ? 'Contact me!' : 'Home'}</p>
+                <p>{location.pathname == '/contact' ? 'Home' : 'Contact me!'}</p>
                 <div>
                     <p className="footer__scroll__text">NEXT PAGE</p>
-                    <div className="footer__scroll__progress"></div>
+                    <div className="footer__scroll__progress">
+                        <div style={{ width: `${counter}%` }}></div>
+                    </div>
                     <img width="20px" src="/assets/img/icon-row-right.svg" alt="icon row" />
                 </div>
             </article>
